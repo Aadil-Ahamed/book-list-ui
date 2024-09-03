79c5462bc64ff4014a40a5dc86bd1d3ce8993e9f
Aadil-Ahamed <aadilismail2024@gmail.com>
2024-09-03 11:44:30 +0530

    Button Working

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@ const listBookButton = document.querySelector('.fetch-books-btn');
 listBookButton.addEventListener('click', () => {
 	console.log('Button clicked');
 })
+
+const addBookButton = document.querySelector('.add-book-btn');
+
+addBookButton.addEventListener('click', () => {
+	console.log('Add book button clicked');
+})
+
